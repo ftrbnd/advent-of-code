025825f108486cad8417fdcba3e27add5450237b
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-12-02 20:10:16 -0800

    Complete day2-part2

--- src/day2/day2.spec.ts
+++ src/day2/day2.spec.ts
@@ -1,9 +1,15 @@
 import day2 from './index';
 
 describe('On Day 2', () =>{
-    it.only(`Part 1: sum of game ids that were possible`, () => {
+    it.skip(`Part 1: sum of game ids that were possible`, () => {
         const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
         
         expect(day2.solveForPartOne(input)).toBe(8);
+    })
+
+    it.only(`Part 1: sum of the powers of sets of cubes`, () => {
+        const input = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\nGame 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\nGame 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\nGame 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\nGame 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+        
+        expect(day2.solveForPartTwo(input)).toBe(2286);
     })
 });
--- src/day2/index.ts
+++ src/day2/index.ts
@@ -64,8 +64,52 @@ class Day2 extends Day {
         return sum;
     }
 
-    solveForPartTwo(input: string): string {
-        return input;
+    solveForPartTwo(input: string): number {
+        let sum = 0;
+        // split each line
+        const games = splitLines(input);
+
+        for (let i = 0; i <= games.length - 1; i++) {
+            const gameId = i + 1;
+            const game = games[i];
+
+            const maxColors = {
+                red: 0,
+                green: 0,
+                blue: 0
+            };
+            
+            // get each game's set
+            const sets = game
+                .replace(`Game ${gameId}: `, '')
+                .split(';')
+                .map(set => set.trim())
+            
+            // go through each set; ex: ['3 blue, 4 red', '1 red, 2 green']
+            for (const set of sets) {
+                // split colors; ex: ['3 blue', '4 red']
+                const colors = set
+                    .split(',')
+                    .map(color => color.trim());
+                
+                // go through each color and check if the amount is greater than the current max
+                for (const color of colors) {
+                    const amount = parseInt(color.split(' ')[0]);
+                    if (color.includes('red') && amount > maxColors.red) {
+                        maxColors.red = amount;
+                    } else if (color.includes('green') && amount > maxColors.green) {
+                        maxColors.green = amount;
+                    } else if (color.includes('blue') && amount > maxColors.blue) {
+                        maxColors.blue = amount;
+                    }
+                }
+            }
+            
+            const power = maxColors.red * maxColors.green * maxColors.blue;
+            sum += power;
+        }
+
+        return sum;
     }
 }
 
