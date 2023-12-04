b4cae023a8c69586a40e67d336f852e38a427fc0
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-12-03 23:15:25 -0800

    Begin day3-part2

--- src/day3/day3.spec.ts
+++ src/day3/day3.spec.ts
@@ -1,9 +1,15 @@
 import day3 from './index';
 
 describe('On Day 3', () =>{
-    it.only(`Part 1: sum of part numbers in engine schematic`, () => {
+    it.skip(`Part 1: sum of part numbers in engine schematic`, () => {
         const input = `467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..`;
 
         expect(day3.solveForPartOne(input)).toBe(4361);
+    })
+
+    it.only(`Part 2: sum of all gear ratios in engine schematic`, () => {
+        const input = `467..114..\n...*......\n..35..633.\n......#...\n617*......\n.....+.58.\n..592.....\n......755.\n...$.*....\n.664.598..`;
+
+        expect(day3.solveForPartTwo(input)).toBe(467835);
     })
 });
--- src/day3/index.ts
+++ src/day3/index.ts
@@ -44,18 +44,27 @@ class Day3 extends Day {
             tempCol++;
         }
 
+        const visited: number[][] = [];
         // replace this number with '.' to prevent duplicates
+        // TODO: this creates problems for part 2
         graph[row] = graph[row]
             .split('')
-            .map((char, index) => index < tempCol && digits.includes(char) ? '.' : char)
+            .map((char, index) => {
+                if (index < tempCol && digits.includes(char)) {
+                    visited.push([row, index]);
+                    return '.';
+                }
+                return char;
+            })
             .join('');
-        
-        return parseInt(number);
+
+        return {
+            currentNumber: parseInt(number),
+        };
     }
     
     solveForPartOne(input: string): number {
-        const lines = splitLines(input).map(line => line.replace('\r', ''))
-        
+        const lines = splitLines(input).map(line => line.replace('\r', ''));
         const partNumbers: number[] = [];
 
         // traverse the schematic
@@ -64,7 +73,7 @@ class Day3 extends Day {
                 // if digit found: search one level around digit for a symbol
                 if (digits.includes(lines[row][col]) && this.adjacentToSymbol(lines, row, col)) {
                     // determine the number we are on
-                    const number = this.determineCurrentNumber(lines, row, col);
+                    const number = this.determineCurrentNumber(lines, row, col).currentNumber;
                     partNumbers.push(number);
                 }
             }
@@ -73,8 +82,45 @@ class Day3 extends Day {
         return partNumbers.reduce((a, b) => a + b);
     }
 
-    solveForPartTwo(input: string): string {
-        return input;
+    // travel around asterisk * and find numbers adjacent to it
+    findAdjacentNumbers(graph: string[], row: number, col: number): number[] {
+        const directions = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, 1], [1, -1], [-1, -1]];
+        const numbers: number[] = []; 
+
+        for (const dir of directions) {
+            const [newRow, newCol] = [row + dir[0], col + dir[1]];
+
+            if (this.withinGraph(graph, newRow, newCol) && digits.includes(graph[newRow][newCol])) {
+                const { currentNumber } = this.determineCurrentNumber(graph, newRow, newCol);
+                numbers.push(currentNumber);
+            }
+        }
+
+        return numbers;
+    }
+
+    solveForPartTwo(input: string): number {
+        const lines = splitLines(input).map(line => line.replace('\r', ''));
+        const gearRatios: number[] = [];
+
+        // loop through lines and search for an asterisk *
+        for (let row = 0; row < lines.length; row++) {
+            for (let col = 0; col < lines[row].length; col++) {
+                if (lines[row][col] === '*') {
+                    // go one level around the asterisk
+                    const numbers = this.findAdjacentNumbers(lines, row, col);
+                    // check if you find exactly 2 adjacent numbers
+                    if (numbers.length === 2) {
+                        // multiple those numbers together to get the gear ratio
+                        const gearRatio = numbers[0] * numbers[1];
+                        gearRatios.push(gearRatio);
+                    }
+                }
+            }
+        }
+
+        // return the sum of all gear ratios
+        return gearRatios.reduce((a, b) => a + b);
     }
 }
 
