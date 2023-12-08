47101256e5876b53d2f8d205ec2fa6d964e1f906
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-12-07 23:55:24 -0800

    Complete day7-part2

--- src/day7/day7.spec.ts
+++ src/day7/day7.spec.ts
@@ -1,9 +1,15 @@
 import day7 from './index';
 
 describe('On Day 7', () =>{
-    it(`Part 1: get the total winnings from hands`, () => {
+    it.skip(`Part 1: get the total winnings from hands`, () => {
         const input = '32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483';
         
         expect(day7.solveForPartOne(input)).toBe(6440);
+    })
+
+    it(`Part 2: get the total winnings from hands with joker rule`, () => {
+        const input = '32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483';
+        
+        expect(day7.solveForPartTwo(input)).toBe(5905);
     })
 });
--- src/day7/index.ts
+++ src/day7/index.ts
@@ -71,6 +71,61 @@ class Day7 extends Day {
         return 0;
     }
 
+    getHandTypeJoker(hand: string): number {
+        const counts = new Map<string, number>();
+
+        for (const card of hand) {
+            const prevCount = counts.get(card) ?? 0;
+            counts.set(card, prevCount + 1);
+        }
+        const jokerCount = counts.get('J') ?? 0;
+
+        switch (counts.size) {
+            case 1:
+                return 7;
+            case 2: {
+                // four of a kind or full house
+                for (const value of counts.values()) {
+                    if (value === 4 || value === 1) {
+                        return hand.includes('J') ? 7 : 6;
+                    } else if (value === 3 || value === 2) {
+                        return hand.includes('J') ? 7 : 5;
+                    }
+                }
+            }
+            case 3: {
+                // three of a kind or two pair
+                for (const value of counts.values()) {
+                    if (value === 3) {
+                        return hand.includes('J') ? 6 : 4;
+                    } else if (value === 2) { // jokerCount can be 1 or 2
+                        return hand.includes('J') ? 4 + jokerCount : 3;
+                    }
+                }
+            }
+            case 4:
+                return hand.includes('J') ? 4 : 2;
+            case 5:
+                return hand.includes('J') ? 2 : 1;
+        }
+
+        return -1;
+    }
+
+    secondOrderCompareJoker(handOne: string, handTwo: string): number {
+        const labels = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'];
+
+        for (let i = 0; i < handOne.length; i++) {
+            const [labelOne, labelTwo] = [handOne[i], handTwo[i]];
+
+            if (labelOne === labelTwo) continue;
+
+            return labels.indexOf(labelOne) < labels.indexOf(labelTwo) ? -1 : 1;
+        }
+
+        return 0;
+    }
+
     solveForPartOne(input: string): number {
         const hands = splitLines(input).map(line => line.split(' ').map(val => val.replace('\r', '')));
 
@@ -78,8 +133,6 @@ class Day7 extends Day {
             const typeDifference = this.getHandType(b[0]) - this.getHandType(a[0]);
             return typeDifference === 0 ? this.secondOrderCompare(a[0], b[0]) : typeDifference;
         })
-
-        console.log('Sorted hands:', hands);
 
         const winnings: number[] = [];
         let rank = hands.length;
@@ -91,7 +144,20 @@ class Day7 extends Day {
     }
 
     solveForPartTwo(input: string): number {
-        return -1;
+        const hands = splitLines(input).map(line => line.split(' ').map(val => val.replace('\r', '')));
+
+        hands.sort((a, b) => {
+            const typeDifference = this.getHandTypeJoker(b[0]) - this.getHandTypeJoker(a[0]);
+            return typeDifference === 0 ? this.secondOrderCompareJoker(a[0], b[0]) : typeDifference;
+        })
+
+        const winnings: number[] = [];
+        let rank = hands.length;
+        for (const [hand, bid] of hands) {
+            winnings.push(parseInt(bid) * rank--);
+        }
+        
+        return winnings.reduce((a, b) => a + b);
     }
 }
 
